ff303d54ceaa43230e3e9676d0a862b4b9bd1086
EdgeworthWright <damhen80@gmail.com>
2019-05-15 17:10:08 +0200

    controller "gemaakt" en wireframes gemaakt

---
+++ private/controllers/EventsController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class EventController
+ *
+ * Deze handelt de logica van ALLE agenda functionaliteit af
+ * Haalt gegevens uit de "model" laag van de website (de gegevens)
+ * Geeft de gegevens aan de "view" laag (HTML template) om weer te geven
+ *
+ */
+class EventsController {
+	function overview(){
+		echo 'Agenda overzichts pagina';
+	}
+}
--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -1,13 +1,11 @@
 <?php
-
 $config = [
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'myband',
     'DB_USER' => 'root',
-    'DB_PASSWORD' => 'root',
+    'DB_PASSWORD' => '',
     'ROOT' => dirname(dirname(__DIR__)),
-    'PRIVATE' => dirname(__DIR__) . '/private',
+    'PRIVATE' => dirname(__DIR__),
     'WEBROOT' => dirname(__DIR__) . '/public',
 ];
-
 return $config;
